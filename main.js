0536c8c067096cf1e2545782b6799c30ad13fe44
Imtiajul <iimtiajul@gmail.com>
2023-08-20 00:57:16 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+import './style.scss'
+import javascriptLogo from './javascript.svg'
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
+
+setupCounter(document.querySelector('#counter'))
+
+
+/*
+
+<li class="item-header"><a href=""><img src="/public/ea-app.png" width="30" alt="ea app">&nbsp; EA
+Games</a>
+</li>
+<li class="item-header"><a href=""><img src="/public/steam.png" width="30" alt="steam">&nbsp;Steam</a>
+</li>
+<li class="item-header"><a href=""><img src="/public/battlenet.png" width="30" alt="battlenet">&nbsp;
+Battle.net</a>
+</li>
+<li class="item-header"><a href=""><img src="/public/ubsoft.png" width="30" alt="ubsoft">&nbsp;Ubisoft</a>
+</li>
+<li class="item-header"><a href=""><img src="/public/rockstart.png" width="30"
+  alt="ea app">&nbsp;Rockstar</a>
+</li>
+<li class="item-header"><a href=""><img src="/public/gog.png" width="30" alt="ea app">&nbsp;GOG.com</a>
+</li>
+<li class="item-header"><a href=""><img src="/public/ms-store.png" width="30" alt="ea app">&nbsp;Microsoft
+</a>
+</li>
+<li class="item-header"><a href=""><img src="/public/png-transparent-epic-games-alt-macos-bigsur-icon.png"
+  width="30" alt="ea app">&nbsp;Epic</a>
+</li>
+
+*/
